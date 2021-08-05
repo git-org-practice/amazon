f6b61a0d7ea279ea0acc45ee7823f9852970af89
Ravikumar4134 <kuttimavi8@gmail.com>
2021-08-05 12:20:46 +0000

    first masssge

---
+++ forloop.sh
@@ -0,0 +1,8 @@
+echo "hello guys this is for loop .."
+for(( a=10 ; a<=20 ; a++ ))
+do
+echo $a
+echo ""
+done
+echo "for loop over"
+
---
+++ read.sh
@@ -0,0 +1,7 @@
+echo "well come to sbi bank "
+read -p "enter your bank user name:" name
+read -sp " enter your bank password:" passwd
+echo ""
+echo "your enterd bank name is :" $name
+echo " your enterd uder password is :" $passwd
+
